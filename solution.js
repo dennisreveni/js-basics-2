57a25eae30c8f50b37c47946a5dc225287f611c0
dennisreveni <dennisreveni@rambler.ru>
2015-01-30 21:14:16 +0200

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,48 +1,53 @@
 function isEven(number) {
-  // % (the remainder operator) can be used to test whether a number is even
-  // or odd by using % 2 to check whether it’s divisible by two. Here’s another way to define
-  // whether a positive whole number is even or odd:
-  //
-  // • Zero is even.
-  // • One is odd.
-
-  // For any other number N, its evenness is the same as N - 2.
-  // Define a recursive function isEven corresponding to this description. The function should accept a number parameter and return a Boolean.
-  // Test it on 50 and 75. See how it behaves on -1. Why? Can you think of a way to fix this?
+    if(number === 0){ //если число четное
+        return true;
+    }else if(number === 1 || isNaN(number)){  //если число не четное, или функция вызвана без аргумента
+        return false;
+    }else if(number < 0){ //если число отрицательное, вызываем эту функцию с противоположным значением
+        return isEven(-number);
+    }else{ // если число больше 2, отнимаем 2 и вызываем эту функцию заново до тех пор, пока не станет 0 или
+        return isEven(number-2);
+    }
 }
 
 function pascal(row, col) {
-  // Write a procedure that computes the elements of Pascal's triangle by means of a recursive process.
-  //     1
-  //    1 1
-  //   1 2 1
-  //  1 3 3 1
-  // 1 4 6 4 1
-  //
-  // pascal(4, 2)
-  // // → 6
+    if (row<col || row===undefined || col===undefined || row<0 || col<0){ // "валидация" введенных значений
+       return undefined;
+    }else if (row == col || col == 0){ // первое и последнее числа равны 1 (википедия)
+       return 1;
+    }else{ // если все в порядке, определяем значение по формуле: n!/m!/(n-m)! (википедия)
+        function factorial(n) {
+            return (n!=1) ? n*factorial(n-1) : 1; // рекурсивный подсчет факториала
+        }
+        return factorial(row) / factorial(col) / factorial(row - col); // расчет значения
+    }
 }
 
-function sqrt(x) {
-  // Square roots by Newton's method http://en.wikipedia.org/wiki/Newton%27s_method
-  // Also you can look at the problem description in SICP[1.1.7] http://newstar.rinet.ru/~goga/sicp/sicp.pdf
-  // You should write a function that takes a number and computes square root
-  // You can build solution based on functions structure defined below or you can make up your own
+// корень квадратный по методу Ньютона - A2=0.5 (A1 + X/A1)
+function sqrt(x) { //основная функция подсчета корня квадрата
+  return (sqrtIter(1, x)); 
 }
 
-function sqrtIter(guess, x) {
+function sqrtIter(guess, x) { //итерация расчета
+  return isGoodEnough(guess, x) ? guess : sqrtIter(improve(guess, x), x); // Если число максимально приближено к результату, возвращаем его, иначе следующая итерация
 }
 
 function improve(guess, x) {
+  return average(guess, (x / guess))
 }
 
 function average(x, y) {
+  return (x + y) / 2;
 }
 
 function isGoodEnough(guess, x) {
+  if (Math.abs(square(guess) - x) / x < 0.0000000000000001) {
+    return true; //Если число максимально приближено к результату, возвращаем true, иначе false
+  }
 }
 
 function square(x) {
+  return x * x;
 }
 
 module.exports.isEven = isEven;
